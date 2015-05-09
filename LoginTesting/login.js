6fce3a563782894f0ad854f2b0ff838ed707348d
SuperSheep18 <nothingwillgowrong@gmail.com>
2015-05-09 11:46:53 -0600

    Steam Login is available and seperate from regular login. Must add the API key to login.js before using it.

--- LoginTesting/login.js
+++ LoginTesting/login.js
@@ -1,24 +1,74 @@
 //Define variables
 var passport = require('passport')
-	, LocalStrategy = require('passport-local').Strategy;
+	, LocalStrategy = require('passport-local').Strategy
+  , SteamStrategy = require('passport-steam').Strategy;
 var http = require('http');
 
 //Express
 var express = require('express');
 var app = express();
 
-//Redirects
-app.post('/login',
-  passport.authenticate('local', { successRedirect: '/success', failureRedirect: '/failure' }));
+//Passport Strategy
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
 
+//Steam Strategy
+passport.use(new SteamStrategy({
+    returnURL: 'http://127.0.0.1:3000/',
+    realm: 'http://127.0.0.1:3000/',
+    apiKey: 'your api key'
+  },
+  function(identifier, profile, done) {
+    User.findByOpenID({ openId: identifier }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
+
+//Define the post strategies for each HTML login form
+app.post('/login',
+  passport.authenticate('local', { successRedirect: '/', failureRedirect: '/failure'} ));
+
+app.post('/login-steam',
+  passport.authenticate('steam', { successRedirect: '/', failureRedirect: '/failure'} ));
+
+//Gets for main page and failure to login
 app.get('/', function (req, res) {
 	res.sendfile('index.html');
 });
 
 app.get('/failure', function (req, res) {
-  res.sendfile('failure.html');
+	res.sendfile('failure.html');
 });
 
+//Gets for Steam Authentication
+app.get('/auth/steam',
+  passport.authenticate('steam'),
+  function(req, res) {
+    // The request will be redirected to Steam for authentication, so
+    // this function will not be called.
+  });
+
+app.get('/auth/steam/return',
+  passport.authenticate('steam', { failureRedirect: '/failure' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+//Serve everything
 var server = app.listen(3000, function () {
 
 	var host = '127.0.0.1';
@@ -28,18 +78,3 @@ var server = app.listen(3000, function () {
 
 });
 
-//Passport Generate
-passport.use(new LocalStrategy(
-	function(username, password, done) {
-		User.findOne({ username: username }, function(err, user) {
-			if (err) { return done(err); }
-			if (!user) {
-				return done(null, false, { message: 'Incorrect username.' });
-			}
-			if (!user.validPassword(password)) {
-				return done(null, false, { message: 'Incorrect password.' });
-			}
-			return done(null, user);
-		});
-	}
-));
