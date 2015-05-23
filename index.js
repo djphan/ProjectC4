cf2f0f162f40dc6a081ebac8dae4da94fe3b7312
Kent Rasmussen <hyprkookeez@gmail.com>
2015-05-23 13:55:27 -0400

    Added some potential styling images, got news working with MongoDB

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
 // Load Modules
 var express = require('express');
-var MobileDetect = require('mobile-detect');
+var mongoose = require('mongoose');
 
 var app = express();
-app.set('view engine', 'jade');
+app.use(express.static('public'));
 
-// Routing
-app.get('/', function (req, res) {
-	res.render('index', req);
+// Database Setup
+var db = mongoose.connection;
+var Schema = mongoose.Schema;
+db.on('error', console.error);
+var newsSchema = new Schema({
+	article: {
+		title: String,
+		author: String,
+		content: String,
+		icon: String,
+		lastUpdate: Date,
+		created: Date
+	}
 });
-app.get('/css/:filename', function (req, res) {
-	var filename = req.params.filename;
-	res.sendFile('/home/kent/ProjectC4/css/' + filename);
+newsSchema.statics.allOfTheArticles = function(callback) {
+	// -_id removes the id from being returned
+	return this.find({}, '-_id', callback);
+};
+var News = mongoose.model('News', newsSchema);
+mongoose.connect('mongodb://localhost/test', function(err, res) {
+	if(err) {
+		console.log("Error connecting to MongoDB. Error: " + err);
+	} else {
+		console.log("Connected to MongoDB!");
+	}
 });
-app.get('/images/:filename', function (req, res) {
-	var filename = req.params.filename;
-	res.sendFile('/home/kent/ProjectC4/images/' + filename);
+
+app.get('/data/ip', function(req, res) {
+	res.json({ ip: req.ip, hostname: req.hostname });
+});
+app.get('/data/news', function(req, res) {
+	var articleList = [];
+	var stuff = News.allOfTheArticles(function(error, articles) {
+		res.json(articles);
+	});
 });
 
 // Start
