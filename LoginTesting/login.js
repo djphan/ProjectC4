605b6323847c5af9111d3e14504db267f6a3f588
Dan <djphan@gmail.com>
2015-05-30 16:07:06 -0600

    Updating Login Module with MongolDB

--- LoginTesting/login.js
+++ LoginTesting/login.js
@@ -1,5 +1,8 @@
 //Define variables
 var config = require('config.json')('./config.json');
+
+// Require Mongosse
+var mongoose = require('mongoose');
 var passport = require('passport')
   , LocalStrategy = require('passport-local').Strategy
   , SteamStrategy = require('passport-steam').Strategy;
@@ -9,10 +12,31 @@ var http = require('http');
 var express = require('express');
 var app = express();
 
+//Database Setup
+var db = mongoose.connection;
+
+var Schema = mongoose.Schema;
+db.on('error', console.error);
+
+var UserSchema = new Schema({
+  username: String,
+  password: String,
+});
+
+var Users = mongoose.model('User', UserSchema);
+
+mongoose.connect('mongodb://127.0.0.1:27017/test', function(err, res) {
+  if(err) {
+    console.log("Error connecting to MongoDB. Error: " + err);
+  } else {
+    console.log("Connected to MongoDB!");
+  }
+});
+
 //Passport Strategy
 passport.use(new LocalStrategy(
   function(username, password, done) {
-    User.findOne({ username: username }, function(err, user) {
+      Users.findOne({ username: username }, function(err, user) {
       if (err) { return done(err); }
       if (!user) {
         return done(null, false, { message: 'Incorrect username.' });
@@ -40,14 +64,18 @@ passport.use(new SteamStrategy({
 
 //Define the post strategies for each HTML login form
 app.post('/login',
-  passport.authenticate('local', { successRedirect: '/', failureRedirect: '/failure'} ));
+  passport.authenticate('local', { successRedirect: '/success', failureRedirect: '/failure'} ));
 
 app.post('/login-steam',
-  passport.authenticate('steam', { successRedirect: '/', failureRedirect: '/failure'} ));
+  passport.authenticate('steam', { successRedirect: '/success', failureRedirect: '/failure'} ));
 
 //Gets for main page and failure to login
 app.get('/', function (req, res) {
   res.sendfile('index.html');
+});
+
+app.get('/success', function (req, res) {
+  res.sendfile('success.html');
 });
 
 app.get('/failure', function (req, res) {
