ee26e3830c6f20aef0c477b6d4ee1e77ba97c3ed
Kent Rasmussen <hyprkookeez@gmail.com>
2015-09-02 21:47:58 -0400

    Rename testing folder

--- testing/setup/setup.sh
+++
@@ -1,8 +0,0 @@
-# NOTE: Script runs from base directory
-
-# Set config to Travis CI
-rm -f config/config.json
-ln -s testing/setup/travis_ci.json config/config.json
-
-# Generate SSL for HTTPS
-openssl req -x509 -newkey rsa:2048 -keyout config/key.pem -out config/cert.pem -days 1825 -nodes -subj '/CN=www.c4esports.com/O=C4 eSports/C=CA'
