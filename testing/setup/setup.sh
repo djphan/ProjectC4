2d913638912ddf331f74ea6bbfd53e5f9eec95a4
Kent Rasmussen <hyprkookeez@gmail.com>
2015-09-02 21:38:36 -0400

    Fix Travis CI Setup

--- testing/setup/setup.sh
+++ testing/setup/setup.sh
@@ -1,8 +1,8 @@
 # NOTE: Script runs from base directory
 
 # Set config to Travis CI
-rm config/default.json
+rm config/config.json
 ln -s config/default.json ../testing/setup/travis_ci.json
 
 # Generate SSL for HTTPS
-openssl req -x509 -newkey rsa:2048 -keyout config/key.pem -out config/cert.pem -days 7 -nodes -subj '/CN=www.c4esports.com/O=C4 eSports/C=CA'
+openssl req -x509 -newkey rsa:2048 -keyout config/key.pem -out config/cert.pem -days 1825 -nodes -subj '/CN=www.c4esports.com/O=C4 eSports/C=CA'
