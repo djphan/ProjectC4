06f41ac233c8b0ee16805680699471cbd98eab8d
Kent Rasmussen <hyprkookeez@gmail.com>
2015-09-02 21:44:06 -0400

    Fix ln issue

--- testing/setup/setup.sh
+++ testing/setup/setup.sh
@@ -1,8 +1,8 @@
 # NOTE: Script runs from base directory
 
 # Set config to Travis CI
-rm config/config.json
-ln -s config/default.json ../testing/setup/travis_ci.json
+rm -f config/config.json
+ln -s testing/setup/travis_ci.json config/config.json
 
 # Generate SSL for HTTPS
 openssl req -x509 -newkey rsa:2048 -keyout config/key.pem -out config/cert.pem -days 1825 -nodes -subj '/CN=www.c4esports.com/O=C4 eSports/C=CA'
